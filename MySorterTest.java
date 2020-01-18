a905d6f3e907aa5d28d01234b921562c73bb7d17
peng <peng316922@gmail.com>
2020-01-18 14:21:46 +0800

    add MySorterTest.java

---
+++ MySorterTest.java
@@ -0,0 +1,41 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MySorterTest {
+	
+	// test set with negative number
+	private double tc7[][] = { { 1, 2, -99 }, { 1, 2, -99 } };
+
+	private MySorter sorter;
+
+	@Before
+	public void setUp() throws Exception {
+		sorter = new MySorter();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		sorter = null;
+	}
+
+	private void assertDoubleArrayEquals(double[] expected, double[] actual) {
+		assertEquals(expected.length, actual.length);
+		for (int i = 0; i < expected.length; i++) {
+			assertEquals(expected[i], actual[i], 0.0001);
+		}
+	}
+
+	@Test
+	public void testSort7() {
+		double expected[] = tc7[1];
+		double input[] = tc7[0];
+		System.out.println("Test Case 7: test set with negative number");
+		input = sorter.sort(input);
+		assertDoubleArrayEquals(expected, input);
+	}
+
+}
